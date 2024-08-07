c9f84d59182bd0f89cd801d1795257230feb0a37
Bonaventura Sanjaya <bonaventura.sanjaya@dexagroup.com>
2024-08-07 22:04:37 +0700

    change profile logout + add analytics

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -2,10 +2,9 @@ import React, { useCallback } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import analytics from '@react-native-firebase/analytics';
 
 import { COLORS } from '@constant';
-import { OnboardingInvestLogo } from '@assets/images';
-import { Typography } from '@components/atom';
 import { Button } from '@components/molecules';
 import { useAuth } from '@store';
 import { redirectOnUnauthorized } from '@utils/helper';
@@ -17,19 +16,16 @@ const Profile = () => {
   const logout = useAuth(state => state.logout);
   const user = useAuth(state => state.user);
 
-  const onLogout = useCallback(() => {
+  const onLogout = useCallback(async () => {
+    await analytics().logEvent('click_logout', { username: user?.username, email: user?.email });
+
     logout()
     redirectOnUnauthorized(null, navigation);
   }, [user, logout, redirectOnUnauthorized, navigation]);
 
   return (
     <View style={styles.container}>
-      <Image source={OnboardingInvestLogo} style={styles.logo} />
-      <Typography type="heading" size="xxlarge" style={styles.text}>
-        Coming Soon
-      </Typography>
-
-      <Button onPress={onLogout}>Logout</Button>
+      <Button style={styles['logout-button']} size='large' onPress={onLogout}>Logout</Button>
     </View>
   );
 };
@@ -40,16 +36,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.neutral100,
-    alignItems: 'center',
+    justifyContent: 'center',
   },
-  logo: {
-    height: 240,
-    width: 240,
-    marginTop: 41.58,
-    marginHorizontal: 60,
-  },
-  text: {
-    marginTop: 20,
-    marginHorizontal: 20,
-  },
+  'logout-button': {
+    marginHorizontal: 20
+  }
 });
